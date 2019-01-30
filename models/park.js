5d6d57aa11a7cd0b1536bdae9aaa3e34f1beeb73
davejmacm <davejmacm@gmail.com>
2019-01-30 09:42:43 +0000

    corrected species

--- models/park.js
+++ models/park.js
@@ -33,7 +33,7 @@ Park.prototype.findSpecies = function (species) {
   const remainingDinosaurs = [];
 
   for (const dinosaur of this.collection) {
-    if (this.collection.species === species) {
+    if (collection.species === species) {
       remainingDinosaurs.push(dinosaur);
     }
   }
